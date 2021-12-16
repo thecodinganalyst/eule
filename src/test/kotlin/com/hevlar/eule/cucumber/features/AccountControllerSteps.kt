c2d95a5702bd13b213095163ff3ed5de31bb5187
Dennis Cai <dennis.cai@zuhlke.com>
2021-12-16 16:13:03 +0800

    update features

--- src/test/kotlin/com/hevlar/eule/cucumber/features/AccountControllerSteps.kt
+++ src/test/kotlin/com/hevlar/eule/cucumber/features/AccountControllerSteps.kt
@@ -9,29 +9,68 @@ import io.cucumber.java8.En
 import org.springframework.beans.factory.annotation.Autowired
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.*
+import org.springframework.http.HttpStatus
+import org.springframework.web.server.ResponseStatusException
 
 class AccountControllerSteps(@Autowired val accountController: AccountController): En {
 
+    var givenAccountList: List<Account> = listOf()
+    var addAccountResults: List<Result<Account>> = listOf()
+    private var mapper: ObjectMapper = JsonMapper.builder().findAndAddModules().build()
+
     init {
-        var accounts: List<Account> = listOf()
-        var mapper: ObjectMapper = JsonMapper.builder().findAndAddModules().build()
+
+        Before { _ ->
+            addAccountResults = listOf()
+        }
 
         When("the following accounts are added"){ table: DataTable ->
-            val rows = table.asMaps()
-            accounts = rows.map {
-                val account = mapper.convertValue(it, Account::class.java)
-                accountController.createAccount(account)
-            }
+            addAccountResults = addAccountsFromDataTable(table)
         }
 
         Then("the following accounts are returned"){ table: DataTable ->
             val rows = table.asMaps()
             rows.mapIndexed { i, it ->
                 val expected = mapper.convertValue(it, Account::class.java)
-                assertThat(expected, samePropertyValuesAs(accounts[i]))
+                assertThat(expected, samePropertyValuesAs(addAccountResults[i].getOrThrow()))
             }
         }
 
+        Given("the following accounts already exist") { table: DataTable ->
+            givenAccountList = addAccountsFromDataTable(table).mapNotNull {
+                it.getOrNull()
+            }
+        }
+
+        Then("HttpStatus {int} is expected") { statusCode: Int ->
+            addAccountResults.any {
+                it.isFailure &&
+                        it.exceptionOrNull() is ResponseStatusException &&
+                        (it.exceptionOrNull() as ResponseStatusException).status == HttpStatus.valueOf(statusCode)
+            }
+        }
+
+    }
+
+    private fun listAccountsFromDatabase(): List<Account>{
+        return accountController.listAccounts()
+    }
+
+    private fun getAccountsFromDataTable(table: DataTable): List<Account>{
+        return table.asMaps().map {
+            mapper.convertValue(it, Account::class.java)
+        }
+    }
+
+    private fun addAccountsFromDataTable(table: DataTable): List<Result<Account>> {
+        val addedAccounts = getAccountsFromDataTable(table).map {
+            try {
+                Result.success(accountController.createAccount(it))
+            }catch (t: Throwable){
+                Result.failure(t)
+            }
+        }
+        return addedAccounts
     }
 
 }
--- src/test/resources/AccountController.feature
+++ src/test/resources/AccountController.feature
@@ -1,8 +1,17 @@
-Feature: AccountController
-  Scenario: Add account
+Feature: Add Account
+  Scenario: Normal behavior
     When the following accounts are added
       | id    | name  | group   | currency | openBal | openDate   |
       | cash  | Cash  | Assets  | SGD      | 100.00  | 2021-01-01 |
     Then the following accounts are returned
       | id    | name  | group   | currency | openBal | openDate   |
       | cash  | Cash  | Assets  | SGD      | 100.00  | 2021-01-01 |
+
+  Scenario: Account already exists
+    Given the following accounts already exist
+      | id    | name  | group   | currency | openBal | openDate   |
+      | cash  | Cash  | Assets  | SGD      | 100.00  | 2021-01-01 |
+    When the following accounts are added
+      | id    | name  | group   | currency | openBal | openDate   |
+      | cash  | Cash  | Assets  | SGD      | 100.00  | 2021-01-01 |
+    Then HttpStatus 409 is expected
