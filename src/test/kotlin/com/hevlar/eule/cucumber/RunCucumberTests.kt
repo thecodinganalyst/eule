00d7a6a0fbc165fa65de06f895a468a2fa446163
Dennis Cai <dennis.cai@zuhlke.com>
2021-12-16 16:15:02 +0800

    refactor features

--- src/test/kotlin/com/hevlar/eule/cucumber/RunCucumberTests.kt
+++ src/test/kotlin/com/hevlar/eule/cucumber/RunCucumberTests.kt
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith
 @RunWith(Cucumber::class)
 @CucumberOptions(
     plugin = ["pretty", "html:build/reports/tests/cucumber/cucumber-report.html"],
-    features = ["src/test/resources/AccountController.feature"]
+    features = ["src/test/resources"]
 )
 class RunCucumberTests {
 }
