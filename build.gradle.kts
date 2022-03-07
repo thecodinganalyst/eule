7bbc2dfa0e87af07fda88b03d8f897b173bc5ef7
Dennis Cai <dennis@thecodinganalyst.com>
2022-03-07 09:13:54 +0800

    add swagger

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.6.0"
+    id("org.springframework.boot") version "2.6.4"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     id("org.jetbrains.kotlin.plugin.noarg") version "1.6.10"
-    kotlin("jvm") version "1.6.0"
-    kotlin("plugin.spring") version "1.6.0"
-    kotlin("plugin.jpa") version "1.6.0"
+    kotlin("jvm") version "1.6.10"
+    kotlin("plugin.spring") version "1.6.10"
+    kotlin("plugin.jpa") version "1.6.10"
     jacoco
 }
 
@@ -25,8 +25,8 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa:2.6.3")
-    implementation("org.springframework.boot:spring-boot-starter-data-rest:2.6.3")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa:2.6.4")
+    implementation("org.springframework.boot:spring-boot-starter-web:2.6.4")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
@@ -35,18 +35,21 @@ dependencies {
     implementation("ch.qos.logback:logback-core:1.2.10")
     implementation("org.slf4j:slf4j-api:1.7.36")
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
-    testImplementation("com.ninja-squad:springmockk:3.1.0")
-    testImplementation("org.springframework.boot:spring-boot-starter-test:2.6.3") {
+    testImplementation("com.ninja-squad:springmockk:3.1.1")
+    testImplementation("org.springframework.boot:spring-boot-starter-test:2.6.4") {
         exclude(module = "mockito-core")
     }
     testImplementation("io.cucumber:cucumber-java8:7.2.3")
     testImplementation("io.cucumber:cucumber-junit:7.2.3")
     testImplementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.1")
     testImplementation(group= "io.cucumber", name="cucumber-spring", version="6.10.4")
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.8.2")
     testRuntimeOnly("org.junit.vintage:junit-vintage-engine:5.8.2")
 
-    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor:2.6.4")
+
+    implementation("org.springdoc:springdoc-openapi-data-rest:1.6.5")
+    implementation("org.springdoc:springdoc-openapi-ui:1.6.6")
+    implementation("org.springdoc:springdoc-openapi-kotlin:1.6.5")
 }
 
 noArg {
